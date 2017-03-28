cbc02c3cd1ace237488a6c2ba6b6a7565d07487c
mttb <mttb@ATS14832.adm.ats.dk>
2017-03-28 23:44:40 +0200

    Main added

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: mttb
+ *
+ * Created on 28. marts 2017, 23:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+void main(void){
+    
+    while(1);
+    
+}
+
